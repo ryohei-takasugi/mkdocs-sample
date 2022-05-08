c5e36aae69787a50ba8a45f46650fb4f4880c8e3
ryohei-takasugi <63960937+ryohei-takasugi@users.noreply.github.com>
2022-05-08 11:54:55 +0900

    [change] changed to kill process before starting service

--- shell/serve.sh
+++ shell/serve.sh
@@ -1,1 +1,3 @@
+docker exec -i mkdocs /bin/ash -c "ps aux | grep serve | grep -v grep  | awk '{print \$1}'"
+docker exec -i mkdocs /bin/ash -c "ps aux | grep serve | grep -v grep  | awk '{print \$1}' | xargs -I% kill %"
 docker exec -i mkdocs /bin/ash -c "cd /root/projects/docs && /usr/bin/mkdocs serve"
